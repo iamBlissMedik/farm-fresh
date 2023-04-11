8151a60d08891c5317d8e4832ead4dacaf342d2a
Ellams Bliss <ellamsbliss@gmail.com>
2023-04-12 00:40:35 +1000

    farm fresh

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+import Navbar from "./components/Navbar.vue";
+</script>
+
+<template>
+  <div class=" w-screen">
+    <Navbar />
+    <div class="px-[121px]">
+      <RouterView />
+    </div>
+  </div>
+</template>
---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  theme: {
+    colors: {
+      farmFresh: "#2B9348",
+      white: "#FFFFFF",
+      black: "#000000",
+    },
+    screens: {
+      sm: "576px",
+      // => @media (min-width: 576px) { ... }
+
+      md: "960px",
+      // => @media (min-width: 960px) { ... }
+
+      lg: "1440px",
+      // => @media (min-width: 1440px) { ... }
+    },
+
+    extend: {},
+  },
+  plugins: [],
+};
